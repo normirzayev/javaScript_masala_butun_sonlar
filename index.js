9032d57357b4d3b79767fc3266471d12bec6413d
abduvalinormirzayev007 <abduvalinormirzayev007@gmail.com>
2022-06-27 20:13:54 +0500

    butun sonlarga doir

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-// 1-misol;
-// let a = 5
-// let p = 4 * a
-// console.log( "tomoni " + a + " ga teng bo'lgan kvadratning peremetri " + p + " ga teng");
-
-// 20-misol
-// var x1 = 5, x2 = 3, y1 = 7, y2 = 9
-// var natija = Math.pow( (Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) , 1/2 )
-// console.log(natija); 
-
-
-// console.log(Math.sqrt());
-
-// let x = 25.5
-// let javob = x / 10
-// let javob2 = x % 10
-
-// console.log( "birinchi xonasi " + parseInt(javob) + " ikinchi xonasi " + parseInt(javob2) + " ga teng");
-
-// 15 butun sonlarga oid 
+// #########-----   Butun sonlarga oid masalalar   ----------########## // 
+// 15-misol butun sonlarga oid 
 // let x = 745, y1 = parseInt(x / 100), y2 = parseInt(x / 10) - (y1 * 10), y3 = parseInt(x % 10) 
 // let javob = y2 * 100 + y1 * 10 + y3 
 // console.log(javob);
 
-// 17 misol
+// 17 misol//
 // let x = 1725, x2 = x % 1000
-
 // console.log(parseInt(x2/100));
-
-
 
 // 24 - misol 
 // let K = 7;
@@ -47,9 +26,8 @@
 // console.log("1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha, 7-ya");
 // console.log("kiritilgan son " + K + ", " + (javob % 7 + 1) + " raqamini yuqoridan qarab olishiz mumkin");
 
-
 // 27-misol
-// let K = 1;
-// let javob = K % 7
-// console.log("1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha, 7-ya");
-// console.log("kiritilgan son " + K + ", " + ((javob + 7) % 7 - 1) + " raqamini yuqoridan qarab olishiz mumkin");
+let K = 1;
+let javob = K % 7
+console.log("1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha, 7-ya");
+console.log("kiritilgan son " + K + ", " + ((javob + 7) % 7 - 1) + " raqamini yuqoridan qarab olishiz mumkin");
