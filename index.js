ee4119fd6908666cce4935b32923e152c20a250e
abduvalinormirzayev007 <abduvalinormirzayev007@gmail.com>
2022-06-30 17:38:35 +0500

    qoshimcha

--- index.js
+++ index.js
@@ -9,19 +9,19 @@
 // console.log(parseInt(x2/100));
 
 // 24 - misol 
-// let K = 7;
+// let K = 11;
 // let javob = K % 7
 // console.log("0-ya, 1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha");
 // console.log(javob);
 
 // 25 - misol 
-// let K = 1;
+// let K = 10;
 // let javob = K % 7
 // console.log("0-ya, 1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha");
 // console.log((javob + 3) % 7 );
 
 // 26-misol
-// let K = 9;
+// let K = 10;
 // let javob = K % 7
 // console.log("1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha, 7-ya");
 // console.log("kiritilgan son " + K + ", " + (javob % 7 + 1) + " raqamini yuqoridan qarab olishiz mumkin");
@@ -33,9 +33,14 @@
 // console.log("kiritilgan son " + K + ", " + ((javob + 5) % 7 + 1) + " raqamini yuqoridan qarab olishiz mumkin");
 
 // 28-misol
-let K = 10;
-let n = 1
-let javob = K % 7
-console.log("yilning boshi " + n + "-raqamdagi kundan boshlanadi ");
-console.log("1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha, 7-ya");
-console.log("kiritilgan son " + K + ", " + ((javob + n-2) % 7 + 1) + " raqamini yuqoridan qarab olishiz mumkin");
+// let K = 10;
+// let n = 1
+// let javob = K % 7
+// console.log("yilning boshi " + n + "-raqamdagi kundan boshlanadi ");
+// console.log("1-du, 2-se, 3-cho, 4-pa, 5-ju, 6-sha, 7-ya");
+// console.log("kiritilgan son " + K + ", " + ((javob + n-2) % 7 + 1) + " raqamini yuqoridan qarab olishiz mumkin");
+
+// 29 - misol
+// let a = 9, b = 9, c = 3, soni = parseInt(a / c) * parseInt(b / c), ortiq_qism = "ortib qolgan S = " + parseInt((a * b) - (c**2 * soni)) 
+// console.log(soni + " ta kv sig'adi");
+// console.log(ortiq_qism + "m kv");
